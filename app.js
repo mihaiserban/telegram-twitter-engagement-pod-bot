9713529f433e821cf46442c0c0bda9ae31d79341
Mihai Serban <mihaiserban2@gmail.com>
2019-01-02 19:12:04 +0200

    add express

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 process.env['NTBA_FIX_319'] = 1;
 
+const app = require('express')();
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
-
-process.on('uncaughtException', function(error) {
-  console.log('\x1b[31m', 'Exception: ', error, '\x1b[0m');
-});
-
-process.on('unhandledRejection', function(error, p) {
-  console.log('\x1b[31m', 'Error: ', error.message, '\x1b[0m');
-});
 
 const Twitter = require('twitter');
 
@@ -164,3 +158,16 @@ const sendToGroup = function(text, html) {
   if (html) bot.sendMessage(chat_id, text, {parse_mode: 'HTML', disable_web_page_preview: 'true'});
   else bot.sendMessage(chat_id, text);
 };
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log('Server started on port ' + PORT);
+});
+
+process.on('uncaughtException', function(error) {
+  console.log('\x1b[31m', 'Exception: ', error, '\x1b[0m');
+});
+
+process.on('unhandledRejection', function(error, p) {
+  console.log('\x1b[31m', 'Error: ', error.message, '\x1b[0m');
+});
